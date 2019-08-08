cb514324de9b8a33bc249f870b9b972193d0c69a
Nathan Rathe <nprathe10@gmail.com>
2019-08-08 23:07:35 +0000

    changed gemspec

---
+++ lib/environment.rb
@@ -0,0 +1,11 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+
+
+require_relative './scraper'
+require_relative './team'
+require_relative './cli'
+require_relative './pl_table_cli/version'
+
--- lib/pl_table_cli.rb
+++
@@ -1,6 +0,0 @@
-require "pl_table_cli/version"
-
-module PlTableCli
-  class Error < StandardError; end
-  # Your code goes here...
-end
