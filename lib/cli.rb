959f7b71d379f7e6facf55a543ee4818f23ac62c
Nathan Rathe <nprathe10@gmail.com>
2019-08-09 06:55:01 +0000

    added to cli and team

--- lib/cli.rb
+++ lib/cli.rb
@@ -2,9 +2,77 @@ class CLI
   def run
     Scraper.new.make_teams
     puts "CLI is running"
+    start
+    # Team.all[1, 20].each do |team|
+    #   puts "#{team.rank} #{team.name}\nfull name - #{team.full_name}\nnickname - #{team.nickname}\nfounded - #{team.founded}\nground - #{team.ground}\ncapacity - #{team.capacity}\nowner - #{team.owner}\nchairman - #{team.chairman}\nmanager - #{team.manager}\n\n\n"
+    # end
+    # puts "#{Team.all[1].name}\nfull name - #{Team.all[1].full_name}\nnick name - #{Team.all[1].nickname}\n#{Team.all[1].founded}\n#{Team.all[1].ground}\n#{Team.all[1].capacity}\n#{Team.all[1].owner}\n#{Team.all[1].chairman}\n#{Team.all[1].manager}\n#{Team.all[1].website}"
+  end
 
-    Team.all[1, 20].each do |team|
-      puts "#{team.rank}.  #{team.name}   #{team.url}"
+  def start
+    puts "Would you like to view the top of the table or the bottom of the table?"
+    puts "(please enter: 'top' or 'bottom')"
+    input = gets.strip.downcase
+
+    if input == "top"
+      input = 1
+      show_table(input)
+    elsif input == "bottom"
+      input = 11
+      show_table(input)
+    else
+      puts "\nPlease enter a valid option.\n\n"
+      start
+    end
+
+
+
+    puts "\n\nWhat team would you like more information on?"
+    puts "(Enter a number from 1 - 20)"
+    input = gets.strip.to_i
+
+    if input >= 1 && input <= 20
+      team_info(input)
+    else
+      while input < 1 || input > 20
+        puts "Please enter a valid number from 1 - 20"
+        input = gets.strip.to_i
+      end
+      team_info(input)
+    end
+
+    puts "thank you"
+    exit
+  end
+
+  def show_table(s)
+    Team.all[s, 10].each do |t|
+      puts "#{t.rank}.  #{t.name}"
     end
   end
+
+  def team_info(r)
+    puts "---------------#{Team.all[r].full_name}---------------"
+    puts "Nickname(s):        #{Team.all[r].nickname}"
+
+    alt_teams = [5, 7, 8, 9, 13, 16, 17, 19, 20]
+    if alt_teams.any? {|n| n == r} == true
+      puts "Ground(stadium):        #{Team.all[r].alt_ground}"
+      puts "Capacity:               #{Team.all[r].alt_capacity}"
+      puts "Founded:                #{Team.all[r].alt_founded}"
+      puts "Owner:                  #{Team.all[r].alt_owner}"
+      puts "Chairman:               #{Team.all[r].alt_chairman}"
+      puts "Manager:                #{Team.all[r].alt_manager}"
+      puts "Website:                #{Team.all[r].alt_website}"
+    else
+      puts "Ground(stadium):        #{Team.all[r].ground}"
+      puts "Capacity:               #{Team.all[r].capacity}"
+      puts "Founded:                #{Team.all[r].founded}"
+      puts "Owner:                  #{Team.all[r].owner}"
+      puts "Chairman:               #{Team.all[r].chairman}"
+      puts "Manager:                #{Team.all[r].manager}"
+      puts "Website:                #{Team.all[r].website}"
+    end
+  end
+
 end
--- lib/team.rb
+++ lib/team.rb
@@ -1,5 +1,5 @@
 class Team
-  attr_accessor :rank, :name, :url
+  attr_accessor :rank, :name, :url, :full_name, :nickname, :founded, :alt_founded, :ground, :alt_ground, :capacity, :alt_capacity, :owner, :alt_owner, :chairman, :alt_chairman, :manager, :alt_manager, :website, :alt_website
   @@all = []
 
   def initialize(rank = nil, name = nil, url = nil)
@@ -23,5 +23,70 @@ class Team
 
   def doc
     @doc ||= Nokogiri::HTML(open(self.url))
+  end
+
+  def full_name
+    @full_name = doc.css('table.infobox.vcard tr:nth-child(2) td').text.strip.delete_suffix("[1]").delete_suffix("[2]")
+  end
+
+  def nickname
+    @nickname = doc.css('table.infobox.vcard tr:nth-child(3) td').text.strip.delete_suffix("[1]").delete_suffix("[2]")
+  end
+
+  def founded
+    @founded = doc.css('table.infobox.vcard tr:nth-child(5) span:nth-child(1)').text.strip.delete_prefix("; ")[0, 13]
+  end
+
+  def alt_founded
+    @alt_founded = doc.css('table.infobox.vcard tr:nth-child(4) span:nth-child(1)').text.strip.delete_prefix("; ")[0, 13]
+  end
+
+  def ground
+    @ground = doc.css('table.infobox.vcard tr:nth-child(6) td').text.strip.delete_suffix("[1]").delete_suffix("[2]")
+  end
+
+  def alt_ground
+    @alt_ground = doc.css('table.infobox.vcard tr:nth-child(5) td').text.strip.delete_suffix("[1]").delete_suffix("[2]")
+  end
+
+  def capacity
+    @capacity = doc.css('table.infobox.vcard tr:nth-child(7) td').text.strip.delete_suffix("[1]").delete_suffix("[2]")
+  end
+
+  def alt_capacity
+    @alt_capacity = doc.css('table.infobox.vcard tr:nth-child(6) td').text.strip.delete_suffix("[1]").delete_suffix("[2]")
+  end
+
+  def owner
+    @owner = doc.css('table.infobox.vcard tr:nth-child(8) td').text.strip.delete_suffix("[1]").delete_suffix("[2]")
+  end
+
+  def alt_owner
+    @alt_owner = doc.css('table.infobox.vcard tr:nth-child(7) td').text.strip.delete_suffix("[1]").delete_suffix("[2]")
+  end
+
+  def chairman
+    @chairman = doc.css('table.infobox.vcard tr:nth-child(9) td').text.strip.delete_suffix("[1]").delete_suffix("[2]")
+  end
+
+  def alt_chairman
+    @alt_chairman = doc.css('table.infobox.vcard tr:nth-child(8) td').text.strip.delete_suffix("[1]").delete_suffix("[2]")
+  end
+
+
+  def manager
+    @manager = doc.css('table.infobox.vcard tr:nth-child(10) td').text.strip.delete_suffix("[1]").delete_suffix("[2]")
+  end
+
+  def alt_manager
+    @alt_manager = doc.css('table.infobox.vcard tr:nth-child(9) td').text.strip.delete_suffix("[1]").delete_suffix("[2]")
+  end
+
+  def website
+    @website = doc.css('table.infobox.vcard tr:nth-child(13) a').attribute('href').text
+  end
+
+  def alt_website
+    @alt_website = doc.css('table.infobox.vcard tr:nth-child(12) a').attribute('href').text
   end
 end
