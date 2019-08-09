138b46dfa6ed1f23fb2076ca2a7712f940dd4dd3
Nathan Rathe <nprathe10@gmail.com>
2019-08-09 21:23:11 +0000

    finished cli

--- lib/cli.rb
+++ lib/cli.rb
@@ -25,9 +25,8 @@ class CLI
       start
     end
 
-
-
-    puts "\n\nWhat team would you like more information on?"
+    puts "\n\n----------------------------------------"
+    puts "What team would you like more information on?"
     puts "(Enter a number from 1 - 20)"
     input = gets.strip.to_i
 
@@ -41,24 +40,40 @@ class CLI
       team_info(input)
     end
 
-    puts "thank you"
-    exit
+    puts "\n\n----------------------------------------"
+    puts "Would you like to view a different team or section of the table? Y/N"
+    input = gets.strip.downcase
+
+    if input == "y"
+      start
+    else
+      puts "\n\nThank you. Best of luck to your favorite team next year!"
+      exit
+    end
   end
 
   def show_table(s)
-    Team.all[s, 10].each do |t|
-      puts "#{t.rank}.  #{t.name}"
+    if s == 1
+      puts "\n\n----------Top of the 2018/19 Premier League Table----------\n\n"
+      Team.all[s, 10].each do |t|
+        puts "#{t.rank}.  #{t.name}"
+      end
+    else
+      puts "\n\n----------Bottom of the 2018/19 Premier League Table----------\n\n"
+      Team.all[s, 10].each do |t|
+        puts "#{t.rank}.  #{t.name}"
+      end
     end
   end
 
   def team_info(r)
-    puts "---------------#{Team.all[r].full_name}---------------"
+    puts "\n\n---------------#{Team.all[r].full_name}---------------\n\n"
     puts "Nickname(s):          #{Team.all[r].nickname}"
     puts "Ground(stadium):      #{Team.all[r].ground}"
     puts "Capacity:             #{Team.all[r].capacity}"
     puts "Founded:              #{Team.all[r].founded}"
-    puts "Owner:                #{Team.all[r].owner}"
-    puts "Chairman:             #{Team.all[r].chairman}"
+    puts "Owner(s):             #{Team.all[r].owner}"
+    puts "Chairman(Chairmen):   #{Team.all[r].chairman}"
     puts "Manager:              #{Team.all[r].manager}"
     puts "Website:              #{Team.all[r].website}"
   end
