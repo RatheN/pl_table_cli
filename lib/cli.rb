571e4e355d2836bde7a3e4714ba56378b57c158e
Nathan Rathe <nprathe10@gmail.com>
2019-08-20 20:09:27 +0000

    cleaning up

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,65 +1,11 @@
 class CLI
   def run
-    # Scraper.new.make_teams
-    # teams = Scraper.scrape_index
-    # Team.make_teams(teams)
-    # puts "Welcome to the 2018/19 Premier League Table."
-    # puts "View different sections of the table and find more information on your favorite teams."
-
-
-
     welcome
     setup
     prompt_for_table
     team_selection
     end_prompt
   end
-
-
-
-
-  # def start
-  #   puts "\n\nWould you like to view the top of the table or the bottom of the table?"
-  #   puts "(please enter: 'top' or 'bottom')"
-  #   input = gets.strip.downcase
-  #
-  #   if input == "top"
-  #     input = 1
-  #     show_table(input)
-  #   elsif input == "bottom"
-  #     input = 11
-  #     show_table(input)
-  #   else
-  #     puts "\nPlease enter a valid option."
-  #     start
-  #   end
-
-    # puts "\n\n----------------------------------------"
-    # puts "What team would you like more information on?"
-    # puts "(Enter a number from 1 - 20)"
-    # input = gets.strip.to_i
-    #
-    # if input >= 1 && input <= 20
-    #   team_info(input)
-    # else
-    #   while input < 1 || input > 20
-    #     puts "Please enter a valid number from 1 - 20"
-    #     input = gets.strip.to_i
-    #   end
-    #   team_info(input)
-    # end
-
-  #   puts "\n\n----------------------------------------"
-  #   puts "Would you like to view a different team or section of the table? Y/N"
-  #   input = gets.strip.downcase
-  #
-  #   if input == "y"
-  #     start
-  #   else
-  #     puts "\n\nThank you. Best of luck to your favorite team next year!"
-  #     exit
-  #   end
-  # end
 
   def welcome
     puts "\n\n----------------------------------------"
